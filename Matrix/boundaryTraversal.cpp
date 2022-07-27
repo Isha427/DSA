95ac345de0b37464111220b74e5e62335fad7a6c
ISHA RASTOGI <isharastogii123@gmail.com>
2022-07-27 23:53:03 +0530

    updated

---
+++ Matrix/boundaryTraversal.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int r=4,c=4;
+void traverse(int arr[r][c])
+{
+   
+}
+
+int main(){
+ int arr[r][c] = {{1, 2, 3, 4},
+                        {5, 6, 7, 8},
+                        {9, 10, 11, 12},
+                        {13, 14, 15, 16}};
+
+       traverse(arr);
+       return 0;
+  return 0;
+}
